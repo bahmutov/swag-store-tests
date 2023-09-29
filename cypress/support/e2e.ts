da940dcdf332740b4a4ec2d2d12e1c464805cd6d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-09-29 16:57:14 -0400

    remove obsolete files

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,8 +1,5 @@
 // first import the 3rd party Cypress plugins
 // to make them available in every command
 
-// @ts-ignore
-require('cypress-watch-and-reload/support')
-
 // import custom commands
 import './commands'
